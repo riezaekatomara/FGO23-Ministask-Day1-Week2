a1d6aef629fccda528011da0243c89498dd73f52
riezaekatomara <130181217+riezaekatomara@users.noreply.github.com>
2025-02-18 15:47:00 +0700

    Delete soal1-day1-week2.js

--- soal1-day1-week2.js
+++
@@ -1,9 +0,0 @@
-function printNumbers(n) {
-  if (typeof n !== "number") return;
-  for (let i = 1; i <= n; i++) {
-    console.log(`${i} `);
-    new Promise((resolve) => setTimeout(resolve, 1000));
-  }
-}
-
-printNumbers(10);
